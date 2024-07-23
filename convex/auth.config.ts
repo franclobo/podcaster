38adbed9ce522b4e5520d460495430968b28da79
franclobo <fjbl2788@gmail.com>
2024-07-23 17:58:22 -0500

    Create podcaster

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://on-mastodon-19.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
